afd972489b1ec834a07c31d9783e21c28f86ec93
Derfellios <joris.nieuwveld@gmail.com>
2022-01-15 17:50:03 +0100

    Add files via upload

--- Client_GameRefresh.lua
+++ Client_GameRefresh.lua
@@ -1,6 +1,6 @@
 function Client_GameRefresh(game)
 
-	if game.Us == nil or (not Mod.PlayerGameData.InitialPopupDisplayed and not (game.Game.TurnNumber > 1 or (game.Game.TurnNumber == 1 and not game.Settings.AutomaticTerritoryDistribution))) then
+	if game.Us ~= nil and (not Mod.PlayerGameData.InitialPopupDisplayed and not (game.Game.TurnNumber > 1 or (game.Game.TurnNumber == 1 and not game.Settings.AutomaticTerritoryDistribution))) then
 
 		UI.Alert("This game includes the Reverse Income mod. If you are in a team of two, your incomes are swapped!")
 		local payload = {};
--- Server_AdvanceTurn.lua
+++ Server_AdvanceTurn.lua
@@ -19,10 +19,12 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 		if #Team == 2 then
 			Income1 = game.Game.PlayingPlayers[Team[1]].Income(0, game.ServerGame.LatestTurnStanding, false, false).Total
 			Income2 = game.Game.PlayingPlayers[Team[2]].Income(0, game.ServerGame.LatestTurnStanding, false, false).Total
-			Difference = Income1 - Income2
-			IncomeMod1 = WL.IncomeMod.Create(Team[1], -Difference, '');
-			IncomeMod2 = WL.IncomeMod.Create(Team[2], Difference, '');
-			addNewOrder(WL.GameOrderEvent.Create(Team[1], game.Game.Players[Team[1]].DisplayName(nil, false) .. ' swapped income with ' .. game.Game.Players[Team[2]].DisplayName(nil, false), {}, {}, nil, {IncomeMod1, IncomeMod2}));
+			IncomeMod11 = WL.IncomeMod.Create(Team[1], -Income1, 'You lose your own income');
+			IncomeMod22 = WL.IncomeMod.Create(Team[2], -Income2, 'You lose your own income');
+			IncomeMod21 = WL.IncomeMod.Create(Team[1], Income2, 'But... you get ' .. game.Game.Players[Team[2]].DisplayName(nil, false) .."'s income in return!");
+			IncomeMod12 = WL.IncomeMod.Create(Team[2], Income1, 'But... you get ' .. game.Game.Players[Team[1]].DisplayName(nil, false) .."'s income in return!");
+			
+			addNewOrder(WL.GameOrderEvent.Create(Team[1], game.Game.Players[Team[1]].DisplayName(nil, false) .. ' swapped income with ' .. game.Game.Players[Team[2]].DisplayName(nil, false), {}, {}, nil, {IncomeMod11, IncomeMod22, IncomeMod21, IncomeMod12}));
 		end
 	end
 end
