aff179f496c7addc939647a351f25754218b447b
Derfellios <joris.nieuwveld@gmail.com>
2022-05-16 23:38:10 +0200

    Add files via upload

--- Client_GameRefresh.lua
+++ Client_GameRefresh.lua
@@ -1,7 +1,6 @@
 function Client_GameRefresh(game)
 
 	if game.Us ~= nil and (not Mod.PlayerGameData.InitialPopupDisplayed and not (game.Game.TurnNumber > 1 or (game.Game.TurnNumber == 1 and not game.Settings.AutomaticTerritoryDistribution))) then
-		if 
 		UI.Alert("This game includes the Reverse Income mod. If you are in a team of two, your incomes are swapped! If the game is a 1v1, your incomes are also swapped!")
 		local payload = {};
 		payload.Message = "InitialPopupDisplayed";
